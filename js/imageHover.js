c88c4ff929a2a7ce0173ca848135b8393e9e2f16
우 <dlsdndls@naver.com>
2025-04-16 14:18:48 +0900

    7주차 완료

--- js/imageHover.js
+++ js/imageHover.js
@@ -1,12 +1,19 @@
-function over(obj) {
+function out(obj) {
+  obj.src = "image/logo_steam.svg";
+  obj.style.width = "150px";
+  obj.style.height = "60px";
+}
+
+// function over(obj) {
+//     obj.src = "image/duck.png";
+//     obj.style.width = "150px";
+//     obj.style.height = "60px";
+//   } 아래처럼 바꿔서 사용가능
+
+
+const over=(obj)=>{
     obj.src = "image/duck.png";
     obj.style.width = "150px";
     obj.style.height = "60px";
-  }
-
-function out(obj) {
-obj.src = "image/logo_steam.svg";
-obj.style.width = "150px";
-obj.style.height = "60px";
-}
+};
   
--- js/pop_up.js
+++ js/pop_up.js
@@ -1,20 +1,32 @@
-function show_clock(){
+
+function pop_up() {
+  var popupWindow = window.open('../popup/popup.html', '팝업창', 'width=600,height=400');
+}
+
+
+// 함수 이름을 clock()으로 사용하고, 자기 자신을 호출하도록 수정
+function clock(){
   let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
   let divClock = document.getElementById('divClock');
   let msg = "현재 시간 : ";
-  if(currentDate.getHours()>12){ // 12시 보다 크면 오후 아니면 오전
-  msg += "오후";
-  msg += currentDate.getHours()-12+"시";
+  if (currentDate.getHours() > 12) { // 12시 보다 크면 오후
+    msg += "오후 " + (currentDate.getHours() - 12) + "시 ";
+  } else {
+    msg += "오전 " + currentDate.getHours() + "시 ";
   }
-  else {
-  msg += "오전";
-  msg += currentDate.getHours()+"시";
-  }
-  msg += currentDate.getMinutes()+"분";
-  msg += currentDate.getSeconds()+"초";
+  msg += currentDate.getMinutes() + "분 " + currentDate.getSeconds() + "초";
   divClock.innerText = msg;
-  if (currentDate.getMinutes()>58) { //정각 1분전 빨강색 출력
-  divClock.style.color="red";
+
+  // 분이 59분에 가까우면 빨간색
+  if (currentDate.getMinutes() > 58) {
+    divClock.style.color = "red";
+  } else {
+    divClock.style.color = "";
   }
-  setTimeout(show_clock, 1000); //1초마다 갱신
-  }
+
+  setTimeout(clock, 1000); // 1초마다 자기 자신 재호출
+}
+
+
+
+  
